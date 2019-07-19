bda9f6b47ae552b9b0dfb311f3d0dca8e2a1cf35
Aromal Sasidharan <aromalsasidharan@gmail.com>
2019-07-19 23:30:45 +0800

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,77 +2,90 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'Scenes/EqualContainer.dart';
 
-
-
-
-
-
 void main() => runApp(MyApp());
 
-Scaffold createScaffold({String title, Widget body}){
+Scaffold createScaffold({String title, Widget body}) {
   return Scaffold(
-        appBar: AppBar(backgroundColor: Colors.blueGrey, title: Text(title)),    
-      body: body,
-    );
+    appBar: AppBar(backgroundColor: Colors.blueGrey, title: Text(title)),
+    body: body,
+  );
 }
 
-
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-        initialRoute: "/",
-        routes: {
-          "/" : (context) => createScaffold(title: "Home",body: Home()),
-          "/strech_container" : (context) => createScaffold(title: "Strech Container",body: EqualContainer()),
-        },
-      );
+      initialRoute: "/",
+      routes: {
+        "/": (context) => createScaffold(title: "Home", body: HomeWidget()),
+        "/strech_container": (context) =>
+            createScaffold(title: "Strech Container", body: EqualContainer()),
+      },
+    );
+  }
+}
+
+class RouteModel {
+  String title;
+  String location;
+  RouteModel({this.title, this.location});
+}
+
+class HomeWidget extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return HomeWidgetState();
   }
 
 }
 
-class RouteModel{
-  String title;
-  String location;
-  RouteModel({this.title,this.location});
-}
+class HomeWidgetState extends State<HomeWidget> {
+  var routes = [
+    RouteModel(title: "Stretch Container", location: '/strech_container'),
+    RouteModel(title: "Partial Fill Container", location: '/strech_container'),
+    RouteModel(title: "Partial", location: '/strech_container'),
+    RouteModel(title: "Partial", location: '/strech_container'),
+    RouteModel(title: "Stretch Container", location: '/strech_container'),
+    RouteModel(title: "Partial Fill Container", location: '/strech_container'),
+    RouteModel(title: "Partial", location: '/strech_container'),
+    RouteModel(title: "Partial", location: '/strech_container'),
+    RouteModel(title: "Stretch Container", location: '/strech_container'),
+    RouteModel(title: "Partial Fill Container", location: '/strech_container'),
+    RouteModel(title: "Partial", location: '/strech_container'),
+    RouteModel(title: "Partial", location: '/strech_container'),
+    RouteModel(title: "Stretch Container", location: '/strech_container'),
+    RouteModel(title: "Partial Fill Container", location: '/strech_container'),
+    RouteModel(title: "Partial", location: '/strech_container'),
+    RouteModel(title: "Partial", location: '/strech_container'),
+    RouteModel(title: "Stretch Container", location: '/strech_container'),
+    RouteModel(title: "Partial Fill Container", location: '/strech_container'),
+    RouteModel(title: "Partial", location: '/strech_container'),
+    RouteModel(title: "Partial", location: '/strech_container'),
+  ];
 
-class Home extends StatelessWidget{
-
-  
-
- @override
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
 
-
-    var expandedChilds = (Widget child) => SizedBox(child: child,width: double.infinity);
+    var itemWidget = (BuildContext context, int position) {
+      return SizedBox(
+          child: RaisedButton(
+              child: Text(routes[position].title),
+              onPressed: () {
+                Navigator.pushNamed(context,routes[position].location);
+              }),
+          width: double.infinity);
+    };
     
-    var routes = [
-      RouteModel(title: "Stretch Container",location: '/strech_container'), 
-      RouteModel(title: "Partial Fill Container",location: '/strech_container'), 
-      RouteModel(title: "Partial",location: '/strech_container'), 
-       RouteModel(title: "Partial",location: '/strech_container'), 
-    ];
+    var listView = ListView.builder(
+      itemBuilder: itemWidget,
+      itemCount: routes.length,
+    ); 
 
-    
-    var raisedButton = (RouteModel item) => RaisedButton(child: Text(item.title), onPressed: () {
-              Navigator.pushNamed(context, item.location);
-          });
-      
-    var columns = Column(
-      
-      children: routes.map(raisedButton).map(expandedChilds).toList()
-      ,
-
+    return Container(child:listView,
+    padding: EdgeInsets.all(20.0),
     );
-
-    var content = Container(
-      alignment: Alignment.center,
-      child: Padding(child: columns, padding: EdgeInsets.all(20.0),
-      ),
-    );
-    return content;
   }
 }
